67a05d9beadfbb94d8a4b515d0fd4f29a6ba26c9
dbatista <dan.silva0512@gmail.com>
2024-10-15 18:40:33 -0300

    15/10

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbatista <dbatista@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 15:28:04 by dbatista          #+#    #+#             */
+/*   Updated: 2024/10/15 18:24:24 by dbatista         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int ft_printf(const char *, ...);
+
+#endif
